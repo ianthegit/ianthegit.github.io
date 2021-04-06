7991f2e3011562a39959a973921c300b495e7bab
ianthegit <70593195+ianthegit@users.noreply.github.com>
2021-04-06 15:43:40 +0100

    Create quizManagerQuestions01.js

---
+++ quizManagerQuestions01.js
@@ -0,0 +1,92 @@
+quests[++questIndexBuilder] = { //
+		name : "Test github",
+		hoverover : "Try out the quicker builder",
+		tags : "test",
+		splash : {splashType : splashTypeText , splashText : "Test github" },
+		questInfo: new Array()
+	};
+
+/*
+  addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeText,
+	question : "",
+	answer : ""} );
+
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypePicture,
+	question : "?",
+	image : "",
+	answer : ""}) ;
+ 
+ 
+ 
+ 
+quests[++questIndexBuilder] = { //
+		name : "Test new quiz builder",
+		hoverover : "Try out the quicker builder",
+		tags : "test",
+		splash : {splashType : splashTypeText / splashTypePictureAndText , splashText : "Test Text", splashImage : "" },
+		questInfo: new Array()
+	};
+
+quests[++questIndexBuilder] = {  //
+		name : "Test new month-based quiz builder",
+		hoverover : "Try out the quicker builder",
+		tags : "test,year,saturday",
+		questInfo: new Array()
+	};
+	
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeText, 	
+	question : "?  " + Table4ColumnStart + "?</td><td>?</td><td>?</td><td>?" + Table4ColumnEnd,
+	answer : "?"	});
+
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeText,
+	question : "",
+	answer : ""    } );
+
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypePicture,
+	question : "What movie is being described",
+	image : "https://lh3.googleusercontent.com/Ftq4kmwf2ZMgm2Au1KHEirPcTYyA2MfoTe5ti0A2v6cPUAyexLjvfFtcovCeJDtF8oX8j6EXHIV9HMrZj41byAmnurxMK4mfRcSw695znXl608VK__rflaCyjX_fcVMXtxvgNfEU0NU=w1920-h1080",
+	answer : "Groundhog Day"
+}) ;
+
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeSpotify,
+	question : 'Name the products these songs were used to advertise',
+	spotify : '<iframe src="https://open.spotify.com/embed/playlist/7BQJ0lhgEX1NCPKBt6TlYm" width="500" height="500" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
+	answer : "Cadbury, Levi's,Muller Light, Galaxy, Wrigleys, Guinness, Vauxhall, Nike, Levi's, Barclaycard,Quality Street  "
+}) ;
+
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeSpotify,
+	question : '',
+	spotify : '<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRm7q0c3dgVMI_pieDAUPcSHXQ8N3I20dHQytGEjaKsq2Pj-l2xHcRpud-McY2UhU_qrBcLyVRidpOJ/embed?start=false&loop=false&delayms=10000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>',
+	answer : "Click the presentation to move it along"
+} );
+
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,
+		question : "In August, who said (during a voice check for a radio broadcast) 'My fellow Americans, I'm pleased to tell you today that I've signed legislation that will outlaw Russia forever. We begin bombing in five minutes'?",
+		answer : "POTUS Ronald Reagan  <iframe width='560' height='315' src='https://www.youtube.com/embed/bBow1ToJBFE' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+}) ;
+
+addQuest(quests[questIndexBuilder],{		type : quizQuestionType2Picture,
+		question : "What movie", //Brigadoon
+		image : "",
+		answerimage : ""
+}) ;
+
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePictureAnswer,
+		question : "Which actor has died most times on the big-screen (ie in films)? <BR/> (a) Danny Trejo, <BR/> (II) Vincent Price, <BR/> (3) Christopher Lee, <BR/> (Ivy) Dennis Hopper.",
+		answerimage : "https://lh3.googleusercontent.com/X8Cwofy-XdhCTIt7Ict9vOQ-Du4KGOMC8wwGSXAP_y9RdlJOiP8RTp3Nnmj5rqik9AV-epVXWxb6hDPjwtbBuHqOur_YQKJrM5P1EoCO3U9U8yFzGXGHvX7El6AE_lK5SsiOB2DR-Dg=w1920-h1080"
+	} ); 
+	
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePictureAnswer,
+	question : "Which actor has died most times on the big-screen (ie in films)? <BR/> (a) Danny Trejo, <BR/> (II) Vincent Price, <BR/> (3) Christopher Lee, <BR/> (Ivy) Dennis Hopper.",
+	answerimage : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Danny_Trejo_by_Gage_Skidmore.jpg/220px-Danny_Trejo_by_Gage_Skidmore.jpg"
+} ); 
+
+questIndexBuilder++;
+
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,
+	question : "who was born?",
+	answer : "Ted"
+} ); 
+
+questIndexBuilder++;
+*/
