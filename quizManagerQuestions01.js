923071d4ab1b66ece23624de753acdc9bf7227f3
ianthegit <70593195+ianthegit@users.noreply.github.com>
2021-08-31 17:40:06 +0100

    Update quizManagerQuestions01.js

--- quizManagerQuestions01.js
+++ quizManagerQuestions01.js
@@ -2523,7 +2523,7 @@ quests[++questIndexBuilder] = { //
 		questInfo: new Array()
 	};
 addQuest(quests[questIndexBuilder],{	type : quizQuestionTypePicture, 	
-	question : "This bridge was meant to reopen in the middle of August 2021 after refurbishment, but got stuck fast and positively refused to swing. In which British city?" + Table4ColumnStart + "Glasgow</td><td>Belfast</td><td>Newcastle</td><td>Cardif" + Table4ColumnEnd,
+	question : "This bridge was meant to reopen in the middle of August 2021 after refurbishment, but got stuck fast and positively refused to swing. In which British city?" + Table4ColumnStart + "Glasgow</td><td>Belfast</td><td>Newcastle</td><td>Cardiff" + Table4ColumnEnd,
 	answer : "Newcastle", image : "https://i.guim.co.uk/img/media/7017c9afe240a893d90c175e89d553dc82e2bed9/0_91_5102_3060/master/5102.jpg?width=620&quality=85&auto=format&fit=max&s=57389415ccf5a566f222c4b7a86e7811"	});
 addQuest(quests[questIndexBuilder],{	type : quizQuestionTypePicture, 	
 	question : "Kathleen Jamie is Scotland’s national poet. What is the role called?" + Table4ColumnStart + "Mauner</td><td>Makar</td><td>Mara</td><td>Mostly boring" + Table4ColumnEnd,
