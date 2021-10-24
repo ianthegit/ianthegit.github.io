fa544bf285e638fa3b6956a58d8f7bffcf1a0c59
ianthegit <70593195+ianthegit@users.noreply.github.com>
2021-10-24 19:41:11 +0100

    Create tieBreakers.js

---
+++ tieBreakers.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+
+
+quests[++questIndexBuilder] = { 
+		name : "TieBreaker 0",
+		hoverover : "Choose a number",
+		tags : "surfers,InnerWheel,saturday,tieBreaker",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeText, 	
+	question : 'Who will win?   Choose a number between 1 and 10. </BR> <iframe width="1020" height="630" src="https://www.youtube.com/embed/oKkMQqBEQdM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+	answer : "Closest to number 8 wins.  If you're still drawing, then it's meant to be"	});
+
+
+quests[++questIndexBuilder] = { 
+		name : "TieBreaker 1",
+		hoverover : "Choose a number",
+		tags : "surfers,InnerWheel,saturday,tieBreaker",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypePicture, 	
+	question : 'How many stones are there left at Stonehenge', image : "https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTYyNzgwNDg4MzQyMTg1ODI1/topic-stonehenge-gettyimages-682586546.jpg",
+	answer : "83"	});
+
+quests[++questIndexBuilder] = { 
+		name : "TieBreaker 2",
+		hoverover : "Choose a number",
+		tags : "surfers,InnerWheel,saturday,tieBreaker",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeText, 	
+	question : 'How many countries are there which have border both the Mediterranean Sea and Atlantic Ocean? ',
+	answer : "3 - France, Spain and Morocco"	});
