080041b956dce0fba3dd74556a726ec79ad357bc
ianthegit <70593195+ianthegit@users.noreply.github.com>
2021-12-01 09:37:24 +0000

    Update quizManagerQuestions02.js

--- quizManagerQuestions02.js
+++ quizManagerQuestions02.js
@@ -1,2 +1,95 @@
 
 
+quests[++questIndexBuilder] = { //https://www.buzzfeed.com/ivyrichardson1/not-nice-sweets-brits
+		name : "Puddin",
+		hoverover : "Do you recognise these?",
+//		splash : {splashType : splashTypeText , splashText : "We've all seen Dame Judy in films, but can you recognise what films are from...?", splashImage : "https://media2.giphy.com/media/YplBZ0pxRAqRyVNVSU/giphy.gif" },
+		tags : "2021-12-5,saturday,InnerWheel,surfers", //,7DegreesOfHBC",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/50c4363d5d1a/sub-buzz-8370-1635096730-11.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto",
+	answer : "Bread & Butter Pudding"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/6e8071cfddbf/sub-buzz-8123-1635096931-9.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Banoffee Pie"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/50c4363d5d1a/sub-buzz-8406-1635097138-3.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Trifle"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/50c4363d5d1a/sub-buzz-8393-1635097227-5.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Pineapple upside-down cake"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/6e8071cfddbf/sub-buzz-8167-1635097420-13.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Mince Pies"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/6e8071cfddbf/sub-buzz-8182-1635097489-25.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Eccles Cakes"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-10/24/17/asset/8bd4a1a7a519/sub-buzz-8204-1635097642-46.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Spotted Dick"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2018-11/26/5/asset/buzzfeed-prod-web-02/sub-buzz-11349-1543228588-1.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Pavlova"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2018-11/26/6/asset/buzzfeed-prod-web-06/sub-buzz-26233-1543231988-2.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Panettone"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2018-11/26/6/asset/buzzfeed-prod-web-03/sub-buzz-11830-1543232397-2.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+	answer : "Stollen"}) ;
+
+
+quests[++questIndexBuilder] = { //https://www.buzzfeed.com/ivyrichardson1/not-nice-sweets-brits   https://www.buzzfeed.com/moonlightskies/how-many-of-these-holiday-films-have-you-seen-alre-aq7jys05t5
+		name : "Goggely-eyed",
+		hoverover : "Do you recognise these?",
+//		splash : {splashType : splashTypeText , splashText : "We've all seen Dame Judy in films, but can you recognise what films are from...?", splashImage : "https://media2.giphy.com/media/YplBZ0pxRAqRyVNVSU/giphy.gif" },
+		tags : "2021-12-5,saturday,InnerWheel,surfers", //,7DegreesOfHBC",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/534c8657e842/enhanced-1331-1637377098-1.png?output-format=auto&output-quality=auto",
+	answer : "The Polar Express"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/09a3e1c6d9ba/enhanced-1317-1637377109-2.png?output-format=auto&output-quality=auto",
+	answer : "A Christmas Story"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/534c8657e842/enhanced-1317-1637377127-9.png?output-format=auto&output-quality=auto",
+	answer : "Scrooged"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/e3c304959981/enhanced-1245-1637377135-4.png?output-format=auto&output-quality=auto",
+	answer : "How the Grinch Stole Christmas"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/8a85010d8fc5/enhanced-1322-1637377175-5.png?output-format=auto&output-quality=auto",
+	answer : "Die Hard"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/8a85010d8fc5/enhanced-1329-1637377187-1.png?output-format=auto&output-quality=auto",
+	answer : "Elf"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/8a85010d8fc5/enhanced-1329-1637377200-7.png?output-format=auto&output-quality=auto",
+	answer : "Miracle on 34th Street"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/8a85010d8fc5/enhanced-1322-1637377208-11.png?output-format=auto&output-quality=auto",
+	answer : "It's a Wonderful LIfe"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/534c8657e842/enhanced-1311-1637377218-7.png?output-format=auto&output-quality=auto",
+	answer : "Home ALone"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/ec854551c070/enhanced-1297-1637377238-13.png?output-format=auto&output-quality=auto",
+	answer : "Love Actually"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/9a43277df4f3/enhanced-1346-1637377248-9.png?output-format=auto&output-quality=auto",
+	answer : "National Lampoon's Christmas Vacation"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/e3c304959981/enhanced-1269-1637377258-1.png?output-format=auto&output-quality=auto",
+	answer : "Bad Santa"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/8a85010d8fc5/enhanced-1329-1637377308-13.png?output-format=auto&output-quality=auto",
+	answer : "White Christmas"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/09a3e1c6d9ba/enhanced-1317-1637377370-14.png?output-format=auto&output-quality=auto",
+	answer : "The Nightmare Before Christmas"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/ec854551c070/enhanced-1297-1637377399-26.png?output-format=auto&output-quality=auto",
+	answer : "The Muppet Christmas Carol"}) ;
+
+
