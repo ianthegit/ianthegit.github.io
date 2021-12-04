638f5951f50142269581c265a6f2b656eac1dfb3
ianthegit <70593195+ianthegit@users.noreply.github.com>
2021-12-04 15:05:58 +0000

    Update quizManagerQuestions02.js

--- quizManagerQuestions02.js
+++ quizManagerQuestions02.js
@@ -42,54 +42,126 @@ addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question :
 quests[++questIndexBuilder] = { //https://www.buzzfeed.com/ivyrichardson1/not-nice-sweets-brits   https://www.buzzfeed.com/moonlightskies/how-many-of-these-holiday-films-have-you-seen-alre-aq7jys05t5
 		name : "Goggely-eyed",
 		hoverover : "Do you recognise these?",
-//		splash : {splashType : splashTypeText , splashText : "We've all seen Dame Judy in films, but can you recognise what films are from...?", splashImage : "https://media2.giphy.com/media/YplBZ0pxRAqRyVNVSU/giphy.gif" },
-		tags : "2021-12-5,saturday,InnerWheel,surfers", //,7DegreesOfHBC",
+		splash : {splashType : splashTypeText , splashText : "We've all seen Christmas films, but can you recognise what these films are...?", splashImage : "https://media2.giphy.com/media/YplBZ0pxRAqRyVNVSU/giphy.gif" },
+		tags : "2021-12-5,saturday,InnerWheel,surfers,7DegreesOfHBC",
 		questInfo: new Array()
 	};
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/534c8657e842/enhanced-1331-1637377098-1.png?output-format=auto&output-quality=auto",
+	image : "https://i.pinimg.com/originals/85/9d/f5/859df5ed37d7f09685c01fb92eca9e2c.gif",
 	answer : "The Polar Express"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/09a3e1c6d9ba/enhanced-1317-1637377109-2.png?output-format=auto&output-quality=auto",
-	answer : "A Christmas Story"}) ;
-addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/534c8657e842/enhanced-1317-1637377127-9.png?output-format=auto&output-quality=auto",
+	image : "https://i.pinimg.com/originals/da/54/08/da54086758a411babec478f0992ea3a5.gif",
 	answer : "Scrooged"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/e3c304959981/enhanced-1245-1637377135-4.png?output-format=auto&output-quality=auto",
+	image : "https://c.tenor.com/IL4a2KC03h8AAAAM/the-ginch-jim-carrey.gif",
 	answer : "How the Grinch Stole Christmas"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/8a85010d8fc5/enhanced-1322-1637377175-5.png?output-format=auto&output-quality=auto",
-	answer : "Die Hard"}) ;
-addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/2/enhanced/8a85010d8fc5/enhanced-1329-1637377187-1.png?output-format=auto&output-quality=auto",
-	answer : "Elf"}) ;
-addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/8a85010d8fc5/enhanced-1329-1637377200-7.png?output-format=auto&output-quality=auto",
-	answer : "Miracle on 34th Street"}) ;
-addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/8a85010d8fc5/enhanced-1322-1637377208-11.png?output-format=auto&output-quality=auto",
-	answer : "It's a Wonderful LIfe"}) ;
-addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/534c8657e842/enhanced-1311-1637377218-7.png?output-format=auto&output-quality=auto",
-	answer : "Home ALone"}) ;
-addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/ec854551c070/enhanced-1297-1637377238-13.png?output-format=auto&output-quality=auto",
+	image : "https://i.gifer.com/embedded/download/Fk4s.gif",
 	answer : "Love Actually"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/9a43277df4f3/enhanced-1346-1637377248-9.png?output-format=auto&output-quality=auto",
+	image : "https://thumbs.gfycat.com/AcademicWarmAustraliancattledog-size_restricted.gif",
+	answer : "Die Hard"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://media1.giphy.com/media/se4DeHuYMeN6bHMEwO/giphy.gif",
+	answer : "Elf"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://c.tenor.com/wONj8eyM0o4AAAAC/miracle-on34th-street-santa-claus.gif",
+	answer : "Miracle on 34th Street"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://c.tenor.com/_BYKmAMVXyQAAAAC/its-a-wonderful-life-snow.gif",
+	answer : "It's a Wonderful LIfe"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://media1.giphy.com/media/zhNNxi6S9fnDnm8PP2/giphy.gif",
+	answer : "Home Alone"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
+	image : "https://media1.giphy.com/media/xUPOqAV1uEEskAX6gw/giphy.gif",
 	answer : "National Lampoon's Christmas Vacation"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/e3c304959981/enhanced-1269-1637377258-1.png?output-format=auto&output-quality=auto",
+	image : "https://media3.giphy.com/media/l0HlEqUKkj18ZFXs4/giphy.gif",
 	answer : "Bad Santa"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/8a85010d8fc5/enhanced-1329-1637377308-13.png?output-format=auto&output-quality=auto",
+	image : "https://media4.giphy.com/media/3ohfFISg9ffrXeXmjC/200.gif",
 	answer : "White Christmas"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/09a3e1c6d9ba/enhanced-1317-1637377370-14.png?output-format=auto&output-quality=auto",
-	answer : "The Nightmare Before Christmas"}) ;
+	image : "https://media3.giphy.com/media/M6DGc0uawRT2g/giphy.gif",
+	answer : "The Nightmare Before Christmas - Starring Helena Bonham Carter"}) ;
 addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What Christmas film is this from?", // 
-	image : "https://img.buzzfeed.com/buzzfeed-static/static/2021-11/20/3/enhanced/ec854551c070/enhanced-1297-1637377399-26.png?output-format=auto&output-quality=auto",
+	image : "https://media3.giphy.com/media/l0ErNev7slIJ2WnTy/giphy.gif",
 	answer : "The Muppet Christmas Carol"}) ;
+
+
+
+
+quests[++questIndexBuilder] = { 
+		name : "Elf-on-the-Shelf",
+		hoverover : "Everyone has heard of Elf-on-the-shelf, but do you know these...?",
+		splash : {splashType : splashTypePictureAndText , splashText : "For instance... Ghost on the Toast", splashImage : "https://i.redd.it/1wmdoikfw2rz.jpg" },
+		tags : "2021-12-5,saturday,InnerWheel,surfers",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://i.redd.it/senttkyw02mz.jpg",
+	answer : "Binks on the Sphinx"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://preview.redd.it/jngzu44r0wlz.jpg?width=640&crop=smart&auto=webp&s=e1e3fbe447c265c3615db398744fa383b2b4dee4",
+	answer : "Shrek on the Deck"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://preview.redd.it/4v23walrgqmz.jpg?width=640&crop=smart&auto=webp&s=11dac38827a3a95d64375ccc4c1477a2642a6bd6",
+	answer : "Putin on Newton"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://i.redd.it/l296xxwcr7261.jpg",
+	answer : "Delorian on the Mandalorian"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://preview.redd.it/lnswgcgj8n161.jpg?width=640&crop=smart&auto=webp&s=f85bde9e7dde3dcb82a444f322e3e2be1ec9eb4e",
+	answer : "Clone on a Stone"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://preview.redd.it/jt5w87oj6lq21.jpg?width=640&crop=smart&auto=webp&s=256001935be18f5ca9db98df8628055b16191797",
+	answer : "Caeser in a Freezer, or Nero at Sub-zero"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://runt-of-the-web.com/wordpress/wp-content/uploads/2017/09/chrome-on-a-gnome.png",
+	answer : "Chrome on a Gnome"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://runt-of-the-web.com/wordpress/wp-content/uploads/2017/09/RuPaul-on-a-U-haul.png",
+	answer : "Ru Paul on a U-Haul"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://runt-of-the-web.com/wordpress/wp-content/uploads/2017/09/fidget-spinner-on-dinner.png",
+	answer : "Spinner on Dinner"}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypePicture,	question : "What is this?", // 
+	image : "https://runt-of-the-web.com/wordpress/wp-content/uploads/2017/09/obama-on-a-llama.png",
+	answer : "Obama on a Lama"}) ;
+
+
+
+
+
+quests[++questIndexBuilder] = { 
+		name : "No Way!",
+		hoverover : "How much do you know about where we are in the world...?",
+		//splash : {splashType : splashTypePictureAndText , splashText : "For instance... Ghost on the Toast", splashImage : "https://i.redd.it/1wmdoikfw2rz.jpg" },
+		tags : "2021-12-5,saturday,InnerWheel,surfers",
+		questInfo: new Array()
+	};
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Is Greenland, compared to Iceland, further..."+ Table4ColumnStart + "West & South</td><td>North & East</td><td>East & West</td><td>North, South, East & West" + Table4ColumnEnd, // 
+	answer : 'North, South, East & West </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12554716.056941109!2d-38.314793545265864!3d76.79725613939398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1638626772248!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "The 3 countries closest to Canada are..."+ Table4ColumnStart + "USA, Russia, Greenland</td><td>USA, Russia, Portugal</td><td>USA, Russia, Iceland</td><td>USA, Russia, France" + Table4ColumnEnd, // 
+	answer : 'USA, Russia, France   Thanks to St Piere and Miquelon</BR> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3916114.3276903243!2d-56.485626636111746!3d47.32977075524781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1638628277708!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "The northernmost point of Brazil is closer to "+ Table4ColumnStart + "Canada</td><td>the Southernmost point of Brazil</td><td>Iceland</td><td>Portugal" + Table4ColumnEnd, // 
+	answer : 'Canada'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Is the Western (Atlantic) entry point of the Panama Canal further West or further East than the Eastern (Pacific) entry point?", // 
+	answer : 'West </BR><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d504290.5506851502!2d-79.80959006896994!3d9.084712135182862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fab5f4b31cd492d%3A0xd9dd11e7a14a0960!2sPanama%20Canal!5e0!3m2!1sen!2suk!4v1638628643744!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> '}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Is the Northen (Mediterranean Sea) entry point to the Suez Canal further North or further South than the Southern (Gulf of Suez) entry point?", // 
+	answer : 'North </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d521298.3140208602!2d32.123220971712016!3d30.86788590383458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f9aaeef52d333b%3A0xadec1b7a1220a846!2sSuez%20Canal!5e0!3m2!1sen!2suk!4v1638628789794!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Which is further East - Chile on the West coast of Southern America or New York on the Eastern coast of North America", // 
+	answer : 'Chile </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d65368830.73108282!2d-80.67996264302417!3d0.18819607722794382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2suk!4v1638628934147!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Do most British people live further north than most Canadians, or most Canadians live further north than most British people?", // 
+	answer : '90% of Canadians live within 20 miles of the USA border, so more British people live further north'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Is Newcastle nearer to Dublin, London or Amsterdam?", // 
+	answer : 'Dublin </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2916985.673220548!2d-1.725544844981024!3d53.103129235608094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1638629305542!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "Alaska holds the title of being the furthest what State of the USA (by Lat and Long)?" + Table4ColumnStart + "Northern</td><td>Northern and Western</td><td>Northern, Western and Eastern</td><td>Northern, Western, Eastern and Southern" + Table4ColumnEnd, // 
+	answer : 'Northern, Western and Eastern </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d30435827.617964!2d-174.24302003028444!3d59.486635365199064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1638629827169!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+addQuest(quests[questIndexBuilder],{		type : quizQuestionTypeText,	question : "If you travel due south from the centre of Motor City, Detroit, which country will you enter first?" + Table4ColumnStart + "Cuba</td><td>Costa Rica</td><td>Colombia</td><td>Canada" + Table4ColumnEnd, // 
+	answer : 'Canada </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d66748.77030372804!2d-83.05605305094491!3d42.323187486104224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824ca0110cb1d75%3A0x5776864e35b9c4d2!2sDetroit%2C%20MI%2C%20USA!5e0!3m2!1sen!2suk!4v1638630128700!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}) ;
+
+
 
 
