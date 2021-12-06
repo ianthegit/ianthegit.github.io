903f0ca00bb77cb16130944f29035b8a5b515ebf
ianthegit <70593195+ianthegit@users.noreply.github.com>
2021-12-06 17:10:36 +0000

    Update tieBreakers.js

--- tieBreakers.js
+++ tieBreakers.js
@@ -33,7 +33,7 @@ quests[++questIndexBuilder] = {
 	};
 addQuest(quests[questIndexBuilder],{	type : quizQuestionTypeText, 	
 	question : 'How many countries are there which border both the Mediterranean Sea and Atlantic Ocean? ',
-	answer : "3 - France, Spain and Morocco"	});
+	answer : '3 - France, Spain and Morocco  </BR> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14822562.389280813!2d-1.4867759361753383!3d40.29735410503691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1638810588001!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'	});
 
 
 quests[++questIndexBuilder] = { 
