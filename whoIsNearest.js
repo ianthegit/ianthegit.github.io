a36b5f3155179d5cd305617aa4f5c141b5f67d94
ianthegit <70593195+ianthegit@users.noreply.github.com>
2023-01-27 13:45:49 +0000

    Update whoIsNearest.js

--- whoIsNearest.js
+++ whoIsNearest.js
@@ -1,37 +1,37 @@
 teamsData =[]
 function createScreen() {
-	answer = getURIString('answer');
-	mustBeUnder = getURIString('mustBeUnder');
-	snippit= ' to ';
-	if (mustBeUnder) {snippit = 'under or equal' + snippit;} 
-	document.write("<div id='grader'  ><table border='0' ><tr><td><span id='ControlFunctions' >Closest " + snippit + answer + " wins.</td></tr><tr><td><input type='button' class='userBtnGo' value='Calculate' title='Calculate' id='Go' onclick='runCalculate()' /> </span></td></tr><tr><td><span id='data' >Data here</span></td></tr></table></div>");
-	setupTeams(getURIString('teamNames').split(','));
-	document.getElementById("data").innerHTML = writeTeamsHTML(false);}
+  answer = getURIString('answer');
+  mustBeUnder = getURIString('mustBeUnder');
+  snippit= ' to ';
+  if (mustBeUnder) {snippit = 'under or equal' + snippit;} 
+  document.write("<div id='grader'  ><table border='0' ><tr><td><span id='ControlFunctions' >Closest " + snippit + answer + " wins.</td></tr><tr><td><input type='button' class='userBtnGo' value='Calculate' title='Calculate' id='Go' onclick='runCalculate()' /> </span></td></tr><tr><td><span id='data' >Data here</span></td></tr></table></div>");
+  setupTeams(getURIString('teamNames').split(','));
+  document.getElementById("data").innerHTML = writeTeamsHTML(false);}
 function runCalculate() {
-	teamsData.sort(function(a,b){return a.sorter-b.sorter;});
-	document.getElementById("data").innerHTML = writeTeamsHTML(true);}
+  teamsData.sort(function(a,b){return a.sorter-b.sorter;});
+  document.getElementById("data").innerHTML = writeTeamsHTML(true);}
 function setupTeams(teams) {
-	teamsData.length = teams.length;
-	for (var i = 0 ; i < teams.length ; i++) { teamsData[i] = {teamName: teams[i], answer: 0, distanceFrom:0, sorter:0}}}
+  teamsData.length = teams.length;
+  for (var i = 0 ; i < teams.length ; i++) { teamsData[i] = {teamName: teams[i], answer: 0, distanceFrom:0, sorter:0}}}
 function writeTeamsHTML(calculated) {
-	teamHTML = "";	counter=0; previous = 1000000;
-	for (var i = 0 ; i < teamsData.length ; i++) {
-		if (calculated) {
-			if (teamsData[i].sorter != null) {
-				if (teamsData[i].sorter != previous) {(counter++) ; previous=teamsData[i].sorter;};
-				teamHTML= teamHTML + "<tr><td>" +  counter + "     </td><td> " + teamsData[i].teamName + "</td><td>    their answer was " + teamsData[i].sorter + " away</td></tr>";}
-		} else { teamHTML= teamHTML + "<tr><td>" + teamsData[i].teamName + "</td><td><input type='number' id='" + teamsData[i].teamName + "' onchange='dataChangedOnRow(" + i + ", this.value)'> </td></tr>";}}
-	return  "<table border = '0'> " + teamHTML + "</table>";}
+  teamHTML = "";	counter=0; previous = 1000000;
+  for (var i = 0 ; i < teamsData.length ; i++) {
+    if (calculated) {
+      if (teamsData[i].sorter != null) {
+        if (teamsData[i].sorter != previous) {(counter++) ; previous=teamsData[i].sorter;};
+        teamHTML= teamHTML + "<tr><td>" +  counter + "     </td><td> " + teamsData[i].teamName + "</td><td>    their answer was " + teamsData[i].sorter + " away</td></tr>";}
+    } else { teamHTML= teamHTML + "<tr><td>" + teamsData[i].teamName + "</td><td><input type='number' id='" + teamsData[i].teamName + "' onchange='dataChangedOnRow(" + i + ", this.value)'> </td></tr>";}}
+  return  "<table border = '0'> " + teamHTML + "</table>";}
 function dataChangedOnRow(rowNo, value) {
-	teamsData[rowNo].answer=value;
-	teamsData[rowNo].distanceFrom = answer - value;
-	teamsData[rowNo].sorter = teamsData[rowNo].distanceFrom;
-	if (teamsData[rowNo].distanceFrom < 0) {
-		teamsData[rowNo].sorter = teamsData[rowNo].distanceFrom *-1;
-		if (mustBeUnder) {teamsData[rowNo].sorter = null;}}}
+  teamsData[rowNo].answer=value;
+  teamsData[rowNo].distanceFrom = answer - value;
+  teamsData[rowNo].sorter = teamsData[rowNo].distanceFrom;
+  if (teamsData[rowNo].distanceFrom < 0) {
+    teamsData[rowNo].sorter = teamsData[rowNo].distanceFrom *-1;
+    if (mustBeUnder) {teamsData[rowNo].sorter = null;}}}
 function getURIString(paramName) {
-	const queryString = window.location.search;
-	const urlParams = new URLSearchParams(queryString);
-	if (urlParams.has(paramName)) {return urlParams.get(paramName);};
-	return '';}
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  if (urlParams.has(paramName)) {return urlParams.get(paramName);};
+  return '';}
 function getTeamNames(teamNamesString){	return decodeURI(teamNamesString);}
