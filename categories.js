654638f42e4bed2f7fd4edfd9f5825da2e9d82c5
ianthegit <70593195+ianthegit@users.noreply.github.com>
2023-04-03 18:11:51 +0100

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,4 +1,44 @@
 
+
+questIndexBuilder = -1;
+quests = new Array();
+
+quests[++questIndexBuilder] = { 
+		name : "Planets ", //+getAndIncrementCounterForType("Alphabet"),
+		hoverover : "Name all non-dwarf planets in our Solar System",
+		timeToCompleteFirstAnswer : 30,
+		tags : "2023-4-2,Planets", 
+		questInfo: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'] };
+
+function createCounter() {
+var countDownDate = new Date("April 3, 2023 17:48:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = minutes + ":" + seconds ;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+}
+
 function createScreen() { teamsData =[] ; answer = getURIString('answer'); mustBeUnder = getURIString('mustBeUnder'); snippit= ' to ';
  setupTeams(getURIString('teamNames').split(','));
  if (mustBeUnder) {snippit = ' under or equal' + snippit;} 
