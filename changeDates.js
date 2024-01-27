2d3bb22f4d93690d2d4dffa04116e7a8e0539587
ianthegit <70593195+ianthegit@users.noreply.github.com>
2024-01-27 15:55:36 +0000

    Update changeDates.js

--- changeDates.js
+++ changeDates.js
@@ -23,13 +23,11 @@ function runCalculateFromDuration() {
 }
 
 function runCalculate(pickedDate) {
- for (var i = 0 ; i < timezones.length ; i++) {
-    document.getElementById(timezones[i].type+'Start').innerHTML = 
-	     formatDate(new Date((new Date(pickedDate) ).toLocaleString("en-US", {timeZone: timezones[i].typeName})));   ;
+ 	for (var i = 0 ; i < timezones.length ; i++) {
+    	document.getElementById(timezones[i].type+'Start').innerHTML = formatDate(new Date((new Date(pickedDate) ).toLocaleString("en-US", {timeZone: timezones[i].typeName})));   ;
 	
-  	endDate = addHours(pickedDate, document.getElementById('duration').value);
-    document.getElementById(timezones[i].type+'End').innerHTML = 
-	     formatDate(new Date((endDate ).toLocaleString("en-US", {timeZone: timezones[i].typeName})));   ;
+  		endDate = addHours(pickedDate, document.getElementById('duration').value);
+    	document.getElementById(timezones[i].type+'End').innerHTML = formatDate(new Date((endDate ).toLocaleString("en-US", {timeZone: timezones[i].typeName})));   ;
     }
 }
 
@@ -47,22 +45,23 @@ function addHours(date, hours) {
 }
 
 function writeTeamsHTML() { teamHTML = "";	
- for (var i = 0 ; i < timezones.length ; i++) {
-    teamHTML= teamHTML + "<tr><td>" + timezones[i].localeName + "</td><td> <span id = " + timezones[i].type + "Start> </span></td><td> to </td><td> <span id = " + timezones[i].type + "End> </span></td></tr>";
-    }
- return  "<table border = '0'> " + teamHTML + "</table>";}
+	for (var i = 0 ; i < timezones.length ; i++) {
+    	teamHTML= teamHTML + "<tr><td>" + timezones[i].localeName + "</td><td> <span id = " + timezones[i].type + "Start> </span></td><td> to </td><td> <span id = " + timezones[i].type + "End> </span></td></tr>";
+ 	}
+ 	return  "<table border = '0'> " + teamHTML + "</table>";
+}
 
 function setupSelector(idName, defaultSelected, selectionChangeFunction, options){
-		var retVal = "";
-		retVal = retVal + '<span id="' + idName + 'Span"> <select name="' + idName + '" id="' + idName + '" onchange="' + selectionChangeFunction + '();" >';
-		optionsCount =options.length;
-		for (var i=0 ; i<optionsCount ; i++){
-        		retVal = retVal + '<option value="' + options[i] + '"' ;
-		    	if (options[i]==defaultSelected){
-			    	retVal = retVal+' selected' ;
-    			}
-                retVal = retVal + '>' + options[i] + '</option>';
-		}
-		retVal = retVal + '</select></span>';
-		return retVal;
+	var retVal = "";
+	retVal = retVal + '<span id="' + idName + 'Span"> <select name="' + idName + '" id="' + idName + '" onchange="' + selectionChangeFunction + '();" >';
+	optionsCount =options.length;
+	for (var i=0 ; i<optionsCount ; i++){
+    	retVal = retVal + '<option value="' + options[i] + '"' ;
+	   	if (options[i]==defaultSelected){
+	    	retVal = retVal+' selected' ;
+    	}
+    	retVal = retVal + '>' + options[i] + '</option>';
+	}
+	retVal = retVal + '</select></span>';
+	return retVal;
 }
