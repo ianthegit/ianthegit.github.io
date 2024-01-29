cf622c0273220ab4c286c7d8a16d44a05820ebe7
ianthegit <70593195+ianthegit@users.noreply.github.com>
2024-01-29 17:51:06 +0000

    bxgfbgf

--- changeDates.js
+++ changeDates.js
@@ -12,7 +12,7 @@ hours=new Array (1,2,3,4,4.5,5,5.5,6,6.5,8,10,12,15,20,24,36,48,'Custom');
 mlist = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 dlist = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 function createScreen(){
-	document.write("<div id='changeSetOrganiser'  ><table border='0' ><tr>"+
+	document.write("<div id='SQQuiz' class='grad' ><table border='0' ><tr>"+
  		"<td> Duration  <span id='durationOuter'>" +setupSelector('duration', 5, 'runCalculateFromDuration', hours)+
  		" </span>hours.</td></tr><tr><td>Start Date and Time (UK Local time) " +
  		" <input type='datetime-local'  value=" + new Date().toLocaleString("en-US") + " title='changeStart' id='startTime' onfocusout='runCalculate(this.value)' /> </td> </tr>" + 
