29bae529f3ab872bc84d6dd8fa63de5defac5b08
ianthegit <70593195+ianthegit@users.noreply.github.com>
2024-01-29 09:40:00 +0000

    bfgdbg

--- changeDates.js
+++ changeDates.js
@@ -7,7 +7,7 @@ timezones.push({type : 'GMT',	localeName : "United Kingdom" , typeName : "Europe
 timezones.push({type : 'EST',	localeName : "Eastern Standard Time" , typeName : "America/New_York" });
 timezones.push({type : 'CST',	localeName : "Central Standard Time" , typeName : "America/Winnipeg" });
 timezones.push({type : 'PST',	localeName : "Pacific Standard Time" , typeName : "America/Los_Angeles" });
-hours=new Array (1,2,3,4,5,5.5,6,6.5,8,10,12,15,20,24,36,48);    
+hours=new Array (1,2,3,4,4.5,5,5.5,6,6.5,8,10,12,15,20,24,36,48);    
 mlist = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 dlist = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
     
