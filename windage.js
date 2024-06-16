4aebaf4ed2f8734245ecfd2a2fae72860b84435c
ianthegit <70593195+ianthegit@users.noreply.github.com>
2024-06-16 14:30:18 +0100

    More complete solution

--- windage.js
+++ windage.js
@@ -1,20 +1,24 @@
 function createScreen() { 
 
-	document.write("<div id='windage'  ><table border='0' >" +
+	document.write("<div id='windage' class='grad' ><table border='0' >" +
+
+
+	"<tr><span id='areaCalcSpan'>"+
+  	"</span></tr>" +
+
 	
-	"<tr><td><span id='areaSpan' >"+
- 
- 	"</span></td></tr>" +
- 
- 	"<tr>  <td>Area </td><td>  <input type='number' id='totalArea' onchange='areaDataChanged(this.value)'> </td>  </tr> " +
+	"<tr><td><span id='areaSpan'>"+
+  	"</span></td></tr>" +
+  	"<tr>  <td>" +
+ 	setupDropdown('areaTypeCalcSpan', 'areaCalcType', 'setAreaTypeCalc' , 0, 	new Array('Total Area', 'Calculate Area' )) +
+ 	" </td><td>  <input type='number' id='totalArea' onchange='areaDataChanged(this.value)'> </td>  <td>M<sup>2</sup></rd> </tr> " +
  	
  	
 	"<tr><td><span id='speedSpan' >"+
- 	"<tr>  <td>Windspeed </td><td>  <input type='number' id='windspeed' onchange='windspeedDataChanged(this.value)'> </td> <td> " +
- 	setupDropdown('windspeedTypeSpan', 'windspeedType', 'setWindspeedType' , 0, 	new Array('Knots', 'MPH', 'Metres per second' )) +
- 	" </td> </tr> " +
- 
- 	"</span></td></tr>" +
+ 	"<tr>  <td>Windspeed </td>" + 
+ 	" <td> " + 	setupDropdown('windspeedTypeSpan', 'windspeedType', 'setWindspeedType' , 0, 	new Array('Knots', 'MPH', 'Metres per second' )) + " </td>" + 
+ 	"<td>  <input type='number' id='windspeed' onchange='windspeedDataChanged(this.value)'> </td>" + 
+ 	" </tr> </span></td></tr>" +
  
  
  	
@@ -27,6 +31,32 @@ function createScreen() {
  	
  	"</table></div>");}
  	
+function setAreaTypeCalc() {
+	if (document.getElementById('areaCalcType').value == 'Total Area') {
+		document.getElementById("areaCalcSpan").innerHTML = "";
+	}
+	if (document.getElementById('areaCalcType').value == 'Calculate Area') {
+		document.getElementById("areaCalcSpan").innerHTML = 
+		"<table><tr><td>Number of 2.4M containers stacked above deck</td> <td><input type='number' id='numberOfContainersHigh' onchange='calculateArea()'> </td> </tr>" +
+		"<tr><td>Deck height in M</td> <td><input type='number' id='deckHeight' onchange='calculateArea()'> </td> </tr>" +
+		"<tr><td>Vessel length in M</td> <td><input type='number' id='vesselLength' onchange='calculateArea()'> </td> </tr></table>"
+		;
+	}
+		 
+ }
+ 	
+function calculateArea() {
+	numberOfContainersHighData = document.getElementById('numberOfContainersHigh').value;
+	deckHeightData = document.getElementById('deckHeight').value;
+	vesselLengthData = document.getElementById('vesselLength').value;
+	calculatedTotalArea = (deckHeightData + (numberOfContainersHighData * 2.4) ) * vesselLengthData;
+	document.getElementById('totalArea').value = calculatedTotalArea;
+	runCalculateWithAreaAndWindspeed(calculatedTotalArea, document.getElementById('windspeed').value)
+	
+}
+
+
+
  function setWindspeedType(windspeedType) {
 	 
 	 runCalculateWithAreaAndWindspeed(document.getElementById('totalArea').value, document.getElementById('windspeed').value)
@@ -43,7 +73,6 @@ function createScreen() {
 
 
 function runCalculateWithAreaAndWindspeed(areaData, windspeedData) {
-	
 	windspeedType = document.getElementById('windspeedType').value;
 	
 	if (windspeedType == 'MPH') {
