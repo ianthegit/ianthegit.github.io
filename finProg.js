ba6107b069aad2375e5eebeb365822fb609a0c44
ianthegit <70593195+ianthegit@users.noreply.github.com>
2024-11-28 12:15:27 +0000

    Update finProg.js

--- finProg.js
+++ finProg.js
@@ -12,7 +12,7 @@ function setupBaseScreen(){
  document.write("<div id='setupData'  ><table border='0' ><tr><td><span id='ControlFunctions' >" + 
  "Basic Details</td></tr>" +
  
- "<tr><td>Personal pension drawdown age</td><td> <input type='number' id='personalPensionAge' onchange='recalculate()'> </td>  	<td>State pension age</td><td> <input type='number' id='statePensionAge' onchange='recalculate()'> </td> </tr>" +
+ "<tr><td>Minimum personal pension drawdown age</td><td> <input type='number' id='personalPensionAge' onchange='recalculate()'> </td>  	<td>State pension age</td><td> <input type='number' id='statePensionAge' onchange='recalculate()'> </td> </tr>" +
  "<tr><td>Tax-free yearly amount</td><td> <input type='number' id='taxFreeAmount' onchange='recalculate()'> </td>  	 </tr>" +
  "<tr><td>Expected growth rate</td><td> <input type='number' id='expectedGrowthRate' onchange='recalculate()'> </td>	<td>Expected inflation rate</td><td> <input type='number' id='expectedInflationRate' onchange='recalculate()'> </td> </tr>" +
  "<tr><td>Your specific details</td></tr>" +
