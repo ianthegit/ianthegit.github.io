c01f69d38e43fb5ebbbb4d24f5cd7ba2d4ff8530
ianthegit <70593195+ianthegit@users.noreply.github.com>
2024-11-29 16:13:04 +0000

    Update finProg.js

--- finProg.js
+++ finProg.js
@@ -10,8 +10,8 @@ function createScreen() {
 
 function setupBaseScreen(){
 	
- document.write("(WARNING - these calculations DO NOT take any income tax due on income into account) </BR> <div id='setupData'  ><table border='0' ><tr><td><span id='ControlFunctions' >" + 
- "Basic Details </td> <td><input type='button' value='Copy URL to Clipboard' title='Copy URL to Clipboard' id='C2C' onclick='createURLAndCopyToClipboard()' /></td></tr>" +
+ document.write("(WARNING - these calculations DO NOT take any income tax due on income into account) <td><input type='button' value='Copy URL to Clipboard' title='Copy URL to Clipboard' id='C2C' onclick='createURLAndCopyToClipboard()' /></td><</BR> <div id='setupData'  ><table border='0' ><tr><td><span id='ControlFunctions' >" + 
+ "Basic Details </td> /tr>" +
  
  "<tr><td>Minimum personal pension drawdown age</td><td> <input type='number' id='personalPensionAge' onchange='recalculate()'> </td>  	<td>State pension age</td><td> <input type='number' id='statePensionAge' onchange='recalculate()'> </td> </tr>" +
  "<tr><td>Tax-free yearly amount</td><td> <input type='number' id='taxFreeAmount' onchange='recalculate()'> </td>  	<td>Max lump sum</td><td> <input type='number' id='maxLumpSum' onchange='recalculate()'> </td>  </tr>" +
