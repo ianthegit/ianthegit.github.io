ae4174bd4afccbdc689c819fbc4f2f73159e14b0
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-01-27 18:31:24 +0000

    queenBee

--- queenBeeLogic.js
+++ queenBeeLogic.js
@@ -2,22 +2,65 @@
  * 
  */
 
- var words = ['abcde', 'efghi', 'hijkl'];
- var primaryLetter = 't';
- const letters = [ 'e','h','y','o','n','p'];
+ //const letters = [ 't', 'e','h','y','o','n','p'];
  
  function checkForWords () {
+	 primaryLetter = getURIString('primaryLetter');
+	 secondaryLetters = getURIStrings('letter');
+	 
+	 letters = [primaryLetter];
+	for (var letterIndex=0 ; letterIndex < secondaryLetters.length ; letterIndex++ ) {
+		letters.push(secondaryLetters[letterIndex])
+	}	 
+	 var solvedList= [];
+	 
+	 document.write("<div id='QueenBeeSolver' >  </div>");
 	 for (var wordIndex=0 ; wordIndex<words.length ; wordIndex++){
-		 wordIterate: if (words[wordIndex].includes(primaryLetter)) {
+		 wordIterate: if (words[wordIndex].includes(primaryLetter) && words[wordIndex].length > 3) {
 			 for (let i = 0; i < words[wordIndex].length; i++) {
-			 	if (!primaryLetter == words[wordIndex][i]  && !letters.includes( words[wordIndex][i] ) ) {
+			 	if (!letters.includes( words[wordIndex][i] ) ) {
 					 break wordIterate;
 				 }
 						 
 			 }
-			 console.log(words[wordIndex]);
+			 solvedList.push(words[wordIndex])
+			 //document.getElementById("QueenBeeSolver").innerHTML = document.getElementById("QueenBeeSolver").innerHTML + words[wordIndex] + '  (' + words[wordIndex].length + ') </BR>';
 		 }	 
 	 }
-	 
-	 
+	 solvedList.sort();
+	 startChar={};
+	 solvedList.forEach(element => {
+  		if (startChar[element.substring(0,2)] === undefined) {
+    		startChar[element.substring(0,2)] = 1;
+  		} else {
+    		startChar[element.substring(0,2)]++;
+  		}
+ 	})
+ 	
+ 	for (let key in startChar) {
+		 document.getElementById("QueenBeeSolver").innerHTML = document.getElementById("QueenBeeSolver").innerHTML + "<span id='" + key + "'> </BR></BR>" + key + "</BR></span>";
+		 for (var wordIndex=0 ; wordIndex<solvedList.length ; wordIndex++){
+			if (solvedList[wordIndex].substring(0,2) == key) {
+				document.getElementById(key).innerHTML = document.getElementById(key).innerHTML + solvedList[wordIndex] + "   ";
+			}	 
+		}
+  		//console.log(`Element ${key} occurs ${countMap[key]} times`);
+	}
  }
+ 
+ function getURIString(paramName) {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (urlParams.has(paramName)) {
+		return urlParams.get(paramName)
+	};
+	return ;
+}
+ function getURIStrings(paramName) {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (urlParams.has(paramName)) {
+		return urlParams.getAll(paramName)
+	};
+	return ;
+}
