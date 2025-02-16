68eba698caeca907981f7da08cec5bd5cf0725c3
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-02-16 14:12:36 +0000

    Update ian.js

--- ian.js
+++ ian.js
@@ -2,4 +2,4 @@
 pics= new Array();
 
 function createScreen() { 
-	document.write("<div id='workstuff' class='grad' ></div>");}
+	document.write("<div id='workstuff' class='grad' ></div></BR></BR> mailto:ian@hopgood.uk </BR></BR>https://www.instagram.com/ianhoppo/");}
