6bf7e6ccf902d2ec9645e73da98faaf4c3963230
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-02-17 16:38:46 +0000

    Update ian.js

--- ian.js
+++ ian.js
@@ -3,8 +3,8 @@ pics= new Array();
 
 function createScreen() { 
 	setupPhotoArray();
-	document.write("<div id='workstuff' class='grad' ></BR></BR> <a href='mailto:ianWho@hopgood.uk'>I won't be very responsive any more by email</a> " 
-	+"</BR></BR><a href='https://www.instagram.com/ianhoppo/'>More pictures</a></div>");
+	document.write("<div id='workstuff' class='grad' ></BR></BR> <a href='mailto:ianWho" + "@hopgood.uk'>I won't be very responsive any more by email</a> " 
+	+"</BR></BR><a href='https://www.instagram.com/" + "ianhoppo/'>More pictures</a></div>");
 	}
 	
 	//document.getElementById('a').style.backgroundImage="url(images/img.jpg)";
