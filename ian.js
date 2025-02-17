d034380142fd40018068f09bffd7c6643c16d5fa
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-02-17 16:55:38 +0000

    Update ian.js

--- ian.js
+++ ian.js
@@ -3,7 +3,7 @@ pics= new Array();
 
 function createScreen() { 
 	setupPhotoArray();
-	document.write("<div id='workstuff' class='grad' ></BR></BR> <a href='mailto:ianWho" + "@hopgood.uk'>I won't be very responsive any more by email</a> " 
+	document.write("<div id='workstuff' class='grad' ></BR></BR> <a href='mailto:ianWho" + "@hopgood.uk?subject=This is not about work, I promise&body=Hi, do you want a beer, I will pay all night'>email</a> " 
 	+"</BR></BR><a href='https://www.instagram.com/" + "ianhoppo/'>More pictures</a></div>");
 	}
 	
@@ -11,6 +11,9 @@ function createScreen() {
 function setupPhotoArray(){
 	photoSubjectIndex=0;
 	
+	
+	pics.push({Dates: "1980-1988", Business:"Hopgoods", Role:"Lingere sales, carpet fitting, deliveries, cashing up, banking", Pictures:new Array(".\pictures\hopgood0.jpg", ".\pictures\hopgood1.jpg",".\pictures\hopgood2.jpg")})
+	pics.push({Dates: "1988-1989", Business:"British Aerospace", Role:"Apprentice riviter, Enemy fire damage application, HQ finance application", Pictures:new Array(".\pictures\brough0.jpg", ".\pictures\brough1.jpg")})
 	
 	
 }
