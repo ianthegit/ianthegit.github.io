18038196e9190f91ef0924eba1a87239926fa78c
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-03-01 14:46:42 +0000

    gfer

--- ian.js
+++ ian.js
@@ -44,7 +44,7 @@ function writePhotoStory(pics) {
 
 function setupPhotoArray(){
 	officePics.push({Dates: "1979-1988", Business:"Hopgoods", Role:"Lingerie sales, carpet fitting, deliveries, cashing up, banking", Pictures:new Array("pictures/hopgoods2.jpg", "pictures/hopgoods1.jpg","pictures/hopgoods0.jpg")})
-	officePics.push({Dates: "1988-1989", Business:"British Aerospace", Role:"Apprentice riviter, Enemy fire damage application, HQ finance application", Pictures:new Array("pictures/brough0.jpg", "pictures/brough1.jpg", "pictures/brough2.jpg")})
+	officePics.push({Dates: "1988-1989", Business:"British Aerospace", Role:"Apprentice riviter, Enemy fire damage application, HQ finance application", Pictures:new Array("pictures/brough0.jpg", "pictures/brough1.jpg", "pictures/brough2.jpg", "pictures/BAEtheStrand.jpg")})
 	officePics.push({Dates: "1989", Business:"Chappell Farm", Role:"Harrower", Pictures:new Array("pictures/harrow.jpg")})
 	officePics.push({Dates: "1989-1994", Business:"Suffolk Country Council - Education Department", Role:"Developer, SCO Admin, Informix DBA", Pictures:new Array("pictures/countyHall0.jpg")})
 	officePics.push({Dates: "1994-2025", Business:"Wellington Underwriting/Catlin/XL Catlin/AXA XL", Role:"Trainee Developer, Technical Architect, Developer, Technical Lead", 
@@ -53,5 +53,5 @@ function setupPhotoArray(){
 		Pictures:new Array("pictures/bangalore0.jpg", "pictures/bangalore2.jpg","pictures/ipswich1.jpg","pictures/catlin4.jpg","pictures/neworleans0.jpg","pictures/pune0.jpg","pictures/pune3.jpg","pictures/sanFrancisco0.jpg","pictures/AXAcouer2.jpg","pictures/shed0.gif","pictures/shed1.jpg","pictures/shed2.jpg")})
 	people.push({Dates: "Social events - beers or bikes for me", Business:"", Role:"", 
 		Pictures:new Array("pictures/brough3.jpg", "pictures/catlin2.jpg", "pictures/catlin3.jpg", "pictures/catlin5.jpg", "pictures/catlin6.jpg", "pictures/catlinTri2.jpg", "pictures/catlinTri0.jpg", "pictures/catlin1.jpg","pictures/hac0.jpg","pictures/Wellington.jpg","pictures/Ipswich2.jpg","pictures/ipswich3.jpg","pictures/bangalore1.jpg",
-		"pictures/bangalore6.jpg","pictures/bangalore3.jpg","pictures/pune1.jpg","pictures/visitors0.jpg","pictures/visitors1.jpg","pictures/axaxl0.jpg","pictures/axaxl1.jpg","pictures/axaxl2.jpg","pictures/axaxl3.jpg","pictures/AXAcouer0.jpg","pictures/AXAcouer1.jpg","pictures/AXAcouer3.png")})
+		"pictures/bangalore6.jpg","pictures/bangalore3.jpg","pictures/pune1.jpg","pictures/visitors0.jpg","pictures/visitors1.jpg", "pictures/gracechurch1.jpg","pictures/axaxl0.jpg","pictures/axaxl1.jpg","pictures/axaxl2.jpg","pictures/axaxl3.jpg","pictures/AXAcouer0.jpg","pictures/AXAcouer1.jpg","pictures/AXAcouer3.png")})
 }
