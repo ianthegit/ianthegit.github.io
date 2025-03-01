83536ab56546e79ec4351e434aa510969a48f060
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-03-01 15:24:07 +0000

    Update ian.js

--- ian.js
+++ ian.js
@@ -30,8 +30,8 @@ function setupPhotoArray(){
 	officePics.push({Dates: "1989-1994", Business:"Suffolk Country Council - Education Department", Role:"Developer, SCO Admin, Informix DBA", Pictures:new Array("pictures/countyHall0.jpg")})
 	officePics.push({Dates: "1994-2025", Business:"Wellington Underwriting/Catlin/XL Catlin/AXA XL", Role:"Trainee Developer, Technical Architect, Developer, Technical Lead", 
 		Pictures:new Array("pictures/museumSt0.jpg", "pictures/mincing0.jpg", "pictures/lloyds0.jpg", "pictures/mincing1.jpg", "pictures/cologne0.jpg", "pictures/gracechurch0.jpg", "pictures/leadenhallStDungeon.jpg", "pictures/ipswich0.jpg", "pictures/shed3.jpg")})
-	officeViews.push({Dates: "My career has given me the opportunity to see some amazing places, sometimes out of the window", Business:"", Role:"", 
+	officeViews.push({Dates: "My career has given me the opportunity to see some amazing places and things, sometimes simply out of the window", Business:"", Role:"", 
 		Pictures:new Array("pictures/bangalore0.jpg", "pictures/bangalore2.jpg","pictures/ipswich1.jpg","pictures/catlin4.jpg","pictures/neworleans0.jpg","pictures/pune0.jpg","pictures/pune3.jpg","pictures/sanFrancisco0.jpg","pictures/AXAcouer2.jpg","pictures/shed0.gif","pictures/shed1.jpg","pictures/shed2.jpg")})
-	people.push({Dates: "Social events - beers or bikes for me", Business:"", Role:"", 
+	people.push({Dates: "Social events - it's always been beers or bikes for me", Business:"", Role:"", 
 		Pictures:new Array("pictures/brough3.jpg", "pictures/catlin2.jpg", "pictures/catlin3.jpg", "pictures/catlin5.jpg", "pictures/catlin6.jpg", "pictures/catlinTri2.jpg", "pictures/catlinTri0.jpg", "pictures/catlin1.jpg","pictures/hac0.jpg","pictures/Wellington.jpg","pictures/Ipswich2.jpg","pictures/ipswich3.jpg","pictures/bangalore1.jpg",
 		"pictures/bangalore6.jpg","pictures/bangalore3.jpg","pictures/pune1.jpg","pictures/visitors0.jpg","pictures/visitors1.jpg", "pictures/gracechurch1.jpg","pictures/axaxl0.jpg","pictures/axaxl1.jpg","pictures/axaxl2.jpg","pictures/axaxl3.jpg","pictures/AXAcouer0.jpg","pictures/AXAcouer1.jpg","pictures/AXAcouer3.png","pictures/london2Paris.jpeg")})}
