1c123ec9d1dfc951ca6c5f70473ef9956cb53808
ianthegit <70593195+ianthegit@users.noreply.github.com>
2025-03-18 16:18:50 +0000

    Update ian.js

--- ian.js
+++ ian.js
@@ -1,11 +1,11 @@
 officePics= new Array();officeViews= new Array();people= new Array();
 if (document.addEventListener) {
   document.addEventListener('contextmenu', function(e) {
-    alert("ChatGPT:  Create me a retirement webpage suitable for the LinkedInLunatics subreddit"); 
+    alert("Are you really doing a code review of me now?  :)"); 
     e.preventDefault(); }, false);
 } else {
   document.attachEvent('oncontextmenu', function() {
-    alert("ChatGPT:  Create me a retirement webpage suitable for the LinkedInLunatics subreddit");
+    alert("Are you really doing a code review of me now? :)");
     window.event.returnValue = false;  });}
 function createScreen() { 
 	setupPhotoArray();
@@ -30,14 +30,14 @@ function writePhotoList(pics) {
 		writePics(pics, i);	}
 	return retString + "<table>";}	
 function setupPhotoArray(){
-	officePics.push({Dates: "1979-1988", Business:"Hopgoods", Role:"Lingerie sales, carpet fitting, deliveries, cashing up, banking", Pictures:new Array("hopgoods2.jpg", "hopgoods1.jpg","hopgoods0.jpg")})
-	officePics.push({Dates: "1988-1989", Business:"British Aerospace", Role:"Apprentice riviter, Enemy fire damage application, HQ finance application", Pictures:new Array("brough0.jpg", "brough1.jpg", "brough2.jpg", "BAEtheStrand.jpg")})
+	officePics.push({Dates: "1979-1988", Business:"Hopgoods", Role:"Carpet sales, lingerie sales, carpet fitting, deliveries, cashing up, banking", Pictures:new Array("hopgoods2.jpg", "hopgoods1.jpg","hopgoods0.jpg")})
+	officePics.push({Dates: "1988-1989", Business:"British Aerospace", Role:"Apprentice riviter, Enemy fire damage application, HQ finance application - Pascal, Fortran, QBasic", Pictures:new Array("brough0.jpg", "brough1.jpg", "brough2.jpg", "BAEtheStrand.jpg")})
 	officePics.push({Dates: "1989", Business:"Chappell Farm", Role:"Harrower", Pictures:new Array("harrow.jpg")})
-	officePics.push({Dates: "1989-1994", Business:"Suffolk Country Council - Education Department", Role:"Developer, SCO Admin, Informix DBA", Pictures:new Array("countyHall0.jpg")})
-	officePics.push({Dates: "1994-2025", Business:"Wellington Underwriting/Catlin/XL Catlin/AXA XL", Role:"Trainee Developer, Technical Architect, Developer, Technical Lead", 
+	officePics.push({Dates: "1989-1994", Business:"Suffolk Country Council - Education Department", Role:"Developer, SCO Admin, Informix DBA: Informix4GL, SQL, Shell scripting", Pictures:new Array("countyHall0.jpg")})
+	officePics.push({Dates: "1994-2025", Business:"Wellington Underwriting/Catlin/XL Catlin/AXA XL", Role:"Trainee Developer, Technical Architect, Developer, Technical Lead: OpenROAD, ABF, Java", 
 		Pictures:new Array("museumSt0.jpg", "mincing0.jpg", "lloyds0.jpg", "mincing1.jpg", "cologne0.jpg", "gracechurch0.jpg", "leadenhallStDungeon.jpg", "ipswich0.jpg", "shed3.jpg")})
 	officeViews.push({Dates: "My career has given me the opportunity to see some amazing places and things, sometimes simply out of the window", Business:"", Role:"", 
 		Pictures:new Array("bangalore0.jpg", "bangalore2.jpg","ipswich1.jpg","catlin4.jpg","neworleans0.jpg","pune0.jpg","pune3.jpg","sanFrancisco0.jpg","AXAcouer2.jpg","shed0.gif","shed1.jpg","shed2.jpg")})
-	people.push({Dates: "Social events - it's always been beers or bikes for me", Business:"", Role:"", 
+	people.push({Dates: "Beers and bikes have always managed to make an appearance", Business:"", Role:"", 
 		Pictures:new Array("brough3.jpg", "catlin2.jpg", "catlin3.jpg", "catlin5.jpg", "catlin6.jpg", "catlinTri2.jpg", "catlinTri0.jpg", "catlin1.jpg","hac0.jpg","Wellington.jpg","Ipswich2.jpg","ipswich3.jpg","bangalore1.jpg",
 		"bangalore6.jpg","bangalore3.jpg","pune1.jpg","visitors0.jpg","visitors1.jpg", "gracechurch1.jpg","axaxl0.jpg","axaxl1.jpg","axaxl2.jpg","axaxl3.jpg","AXAcouer0.jpg","AXAcouer1.jpg","AXAcouer3.png","london2Paris.jpeg")})}
